0e3b940276db10d5b2ec162fb5b0c7e4c64e6bb1
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-07 19:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function happyHolidayTo(holiday, name){
 }
 
 
-function holidayCountdown (holiday , holiday){
+function holidayCountdown (holiday , days){
   
   return `It's ${days} days until ${holiday}!`
 }
